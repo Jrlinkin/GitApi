4d5d5b0333b6ceef4a18f3baee55d7484e1845b5
Jrlinkin <erick-ras@hotmail.com>
2022-07-06 16:32:34 -0300

    Adiciona repositorios e starred
    	Adiciona a funcionalidade que exibe os repositorios e o starred
    	de cada usuario.

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,35 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useGithub from "../../hooks/github-hooks";
 import * as S from './styled';
 import RepositoryItem from "../repository-item";
 
 const Repositories = () => {
+
+    const { githubState, getUserRepos, getUserStarred} = useGithub();
+
+    const [hasUserForSearchRepos,setHasUserForSearchRepos] = useState(false);
+
+    useEffect(()=>{
+
+        if(!!githubState.user.login){
+            getUserRepos(githubState.user.login);
+            getUserStarred(githubState.user.login);
+        }
+        setHasUserForSearchRepos(!!githubState.repositories)
+        
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [githubState.user])
+
+
+
     return (
-        <S.WrapperTabs
-        selectedTabClassName="is-selected" 
-        selectedTabPanelClassName="is-selected"
-        >
-            <S.WrapperTabList>
-                <S.WrapperTab>Repositories</S.WrapperTab>
-                <S.WrapperTab>Starred</S.WrapperTab>
-            </S.WrapperTabList>
-            
-            <S.WrapperTabPanel>
-                 <RepositoryItem 
-                    name='GITAPI'
-                    fullName='Jrlinkin/GitApi'
-                    linkToRepo='https://github.com/Jrlinkin/GitApi'
-                 />
-            </S.WrapperTabPanel>
+        <>
+            {hasUserForSearchRepos ? ( 
+            <S.WrapperTabs
+            selectedTabClassName="is-selected" 
+            selectedTabPanelClassName="is-selected"
+            >
+                <S.WrapperTabList>
+                    <S.WrapperTab>Repositories</S.WrapperTab>
+                    <S.WrapperTab>Starred</S.WrapperTab>
+                </S.WrapperTabList>
+                
+                <S.WrapperTabPanel>
+                    {githubState.repositories.length !== 0 ?
+                       githubState.repositories.map((item) => (
+                          <RepositoryItem 
+                          key={item.id}
+                          name={item.name}
+                          fullName={item.full_name}
+                          linkToRepo={item.html_url}
+                      />
+                    )) :  <p>Este usuario não possui repositorios ainda!</p> }
+                </S.WrapperTabPanel>
 
-            <S.WrapperTabPanel>
-            <RepositoryItem 
-                    name='Lista-de-tarefas'
-                    fullName='Jrlinkin/Lista-de-tarefas'
-                    linkToRepo='https://github.com/Jrlinkin/Lista-de-tarefas'
-                 />
-            </S.WrapperTabPanel>
+                <S.WrapperTabPanel>
+                {githubState.starred.length !== 0 ?
+                githubState.starred.map((item) => (
+                    <RepositoryItem 
+                        key={item.id}
+                        name={item.name}
+                        fullName={item.full_name}
+                        linkToRepo={item.html_url}
+                        />
+                    )) :  <p>Este usuario não possui starred ainda!</p> }
+                </S.WrapperTabPanel>
 
-        </S.WrapperTabs>
+            </S.WrapperTabs>
+            ) : (
+                <></>
+            )}
+        
+        </>
     )
 }
 
--- src/providers/github-provider.js
+++ src/providers/github-provider.js
@@ -14,6 +14,7 @@ const GithubProvider = ( { children } ) =>{
         hasUser:false,
         loading: false,
         user:{
+            id:undefined,
             login: undefined,
             name: undefined,
             html_url: undefined,
@@ -44,6 +45,7 @@ const GithubProvider = ( { children } ) =>{
                 ...prevState, 
                 hasUser:true,
                 user: {
+                    id:data.id,
                     login: data.login,
                     name: data.name,
                     html_url: data.html_url,
@@ -65,9 +67,35 @@ const GithubProvider = ( { children } ) =>{
         })
     }
 
+    const getUserRepos = (username) => {
+
+         api.get(`users/${username}/repos`).then(({ data }) => {
+            console.log('REPOSITORIOSsdfsadf', data);
+            setGithubState(prevState => ({
+                ...prevState,
+                repositories: data,
+            }))
+        })
+    }
+
+    const getUserStarred = (username) => {
+
+        api.get(`users/${username}/starred`).then(({ data }) => {
+           console.log('STARRED', data);
+           setGithubState(prevState => ({
+               ...prevState,
+               starred: data,
+           }))
+       })
+   }
+
     const contextValue = {
         githubState,
         getUser: useCallback((username) => getUser(username), []),
+        getUserRepos: useCallback((username) => getUserRepos(username), []),
+        getUserStarred: useCallback((username) => getUserStarred(username), []),
+
+
     }
 
     return(
